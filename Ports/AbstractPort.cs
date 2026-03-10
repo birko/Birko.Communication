8c09fad2d3b85327100ae7b1e26090c338231781
František Bereň <birko@live.com>
2026-03-10 11:05:57 +0100

    Add project files and implement core port communication classes

---
+++ Ports/AbstractPort.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birko.Communication.Ports
+{
+
+    public class PortSettings
+    {
+        public string Name { get; set; }
+
+        public virtual string GetID()
+        {
+            return string.Format("AbstratPort|{0}", Name);
+        }
+    }
+
+    /// <summary>
+    /// Abstract class for port communication implements IPort interface
+    /// </summary>
+    ///
+    public abstract class AbstractPort : IPort
+    {
+        /// <summary>
+        /// Gets or sets the read data buffer list.
+        /// </summary>
+        /// <value>The read data buffer list.</value>
+        public List<byte> ReadData { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the settings.
+        /// </summary>
+        /// <value>The settings.</value>
+        public PortSettings Settings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the process data delegate.
+        /// </summary>
+        /// <value>The process data delegate.</value>
+        private event ProcessDataDelegate OnProcessData;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether port is open.
+        /// </summary>
+        /// <value><c>true</c> if this port is open; otherwise, <c>false</c>.</value>
+        protected bool _isOpen { get; set; } = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbstractPort"/> class.
+        /// </summary>
+        public AbstractPort()
+        {
+            ReadData = new List<Byte>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbstractPort"/> class.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        public AbstractPort(PortSettings settings) : this()
+        {
+            Settings = settings;
+        }
+
+        public abstract void Write(byte[] data);
+
+        public abstract byte[] Read(int size);
+
+        public abstract void Open();
+
+        public abstract void Close();
+
+        public abstract bool HasReadData(int size);
+
+        public abstract byte[] RemoveReadData(int size);
+
+        public virtual bool IsOpen()
+        {
+            return _isOpen;
+        }
+
+        public virtual void Clear()
+        {
+            ReadData.Clear();
+        }
+
+        public virtual bool IsEmpty()
+        {
+            return (ReadData.Count == 0);
+        }
+
+        public virtual byte[] GetData()
+        {
+            return ReadData.ToArray();
+        }
+
+        public void SubscribeProcessData(ProcessDataDelegate action)
+        {
+            OnProcessData += action;
+        }
+
+        public void UnSubscribeProcessData(ProcessDataDelegate action)
+        {
+            OnProcessData -= action;
+        }
+
+        public void InvokeProcessData()
+        {
+            OnProcessData?.Invoke();
+        }
+    }
+}
---
+++ Ports/IPort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birko.Communication.Ports
+{
+
+    /// <summary>
+    /// delegate called after the port has sended data
+    /// </summary>
+    public delegate void ProcessDataDelegate();
+
+    public interface IPort
+    {
+        /// <summary>
+        /// Writes the specified data.
+        /// </summary>
+        /// <param name="data">The data as byte array.</param>
+        void Write(byte[] data);
+
+        /// <summary>
+        /// Reads the specified size fom port.
+        /// </summary>
+        /// <param name="size">The size.</param>
+        /// <returns>Byte array witdh data</returns>
+        byte[] Read(int size);
+
+        /// <summary>
+        /// Opens port instance.
+        /// </summary>
+        void Open();
+
+        /// <summary>
+        /// Closes port instance.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Determines whether port [has read data] [the specified size].
+        /// </summary>
+        /// <param name="size">The size.</param>
+        /// <returns>
+        /// 	<c>true</c> if port [has read data] [the specified size]; otherwise, <c>false</c>.
+        /// </returns>
+        bool HasReadData(int size);
+
+        /// <summary>
+        /// Removes the read data from port data buffer.
+        /// </summary>
+        /// <param name="size">The size.</param>
+        /// <returns>Removed data as byte array</returns>
+        byte[] RemoveReadData(int size);
+
+        bool IsOpen();
+
+        void Clear();
+
+        bool IsEmpty();
+
+        byte[] GetData();
+
+        void SubscribeProcessData(ProcessDataDelegate action);
+        void UnSubscribeProcessData(ProcessDataDelegate action);
+    }
+}
